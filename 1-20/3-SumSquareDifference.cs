911873ab61d9d0e8652bf8be60f90c5528d58d72
LuisitoRizado <laog.sonic4@gmail.com>
2025-02-03 17:32:02 -0600

    [ProjectEuler] SumSquareDifference Problem Added

---
+++ 1-20/3-SumSquareDifference.cs
@@ -0,0 +1,35 @@
+﻿
+namespace ProjectEulerProblems._1_20
+{
+    public class SumSquareDifference
+    {
+        //DESCRIPTION: 
+        //The sum of the squares of the first ten natural numbers is
+        //1^2 + 2^2 +... +10^2 = 385
+        //The square of the sum of the first ten natural numbers is,
+        //(1 + 2 + ... +10)^2 = 3025
+
+        //Hence the difference between the sum of the squares of the first ten natural
+        //numbers and the square of the sum is 3025 - 385 = 2640
+        
+        //SOLVE:
+        //Find the difference between the sum of the squares of the
+        //first one hundred natural numbers and the square of the sum.
+
+        public static double CalculateSumSquareDifference(int limit)
+        {
+            int totalDifference = 0;
+            double squareNumbersSum = 0;
+           
+            //First calculate the sum of the square of each number
+            for (int i = 0; i <= limit; i++)
+                squareNumbersSum += Math.Pow(i,2);
+
+            double acumulatedSumSquare = Math.Pow((limit * (limit + 1) / 2),2);
+
+
+            return acumulatedSumSquare - squareNumbersSum ;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,3 +5,5 @@ using ProjectEulerProblems._1_20;
 Console.WriteLine(MultiplesOf3Or5.SumOfMultiplesBelow1000());
 //2.- Pair numbers sum in fibonacci sequence
 Console.WriteLine(FibonacciNumber.PairNumbersSumInFibonacciSecuence());
+//3- Sum Square Difference
+Console.WriteLine(SumSquareDifference.CalculateSumSquareDifference(100));
