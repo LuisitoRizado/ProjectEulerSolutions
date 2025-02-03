4e0c4360e6dd68e9a4c05e4d8f569c26cea33a8a
LuisitoRizado <laog.sonic4@gmail.com>
2025-02-03 17:00:31 -0600

    [ProjectEulerl] First two problems added

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ProjectEulerProblems._1_20;
+
+//1.- Multiples of 3 or 5
+Console.WriteLine(MultiplesOf3Or5.SumOfMultiplesBelow1000());
+//2.- Pair numbers sum in fibonacci sequence
+Console.WriteLine(FibonacciNumber.PairNumbersSumInFibonacciSecuence());
